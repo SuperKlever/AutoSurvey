dc7aa95aba9c0881453609fa2cea438ba7831aa3
SuperKlever <selik.2@mail.ru>
2025-02-11 12:23:58 +0300

    Init

---
+++ Main.cs
@@ -0,0 +1,35 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(AutoSurvey.Main), "Auto Map Survey", "1.0.0", "SuperKlever", null)]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
+
+namespace AutoSurvey
+{
+    public class Main : MelonMod
+    {
+		public static float lastDrawTime = 0f;
+
+		public override void OnInitializeMelon()
+        {
+			Settings.OnLoad();
+		}
+
+		public override void OnUpdate()
+		{
+			if (!GameManager.IsMainMenuActive() &&
+				Settings.Instance.autodrawEnabled &&
+				!InterfaceManager.IsPanelEnabled<Panel_Map>())
+			{
+				Main.lastDrawTime += Time.deltaTime;
+				if (Main.lastDrawTime >= Settings.Instance.autodrawDelay)
+				{
+					InterfaceManager.GetPanel<Panel_Map>().DoDetailSurvey();
+					InterfaceManager.GetPanel<Panel_Map>().CloseSelf();
+					Main.lastDrawTime = 0f;
+				}
+			}
+		}
+	}
+}
---
+++ Settings.cs
@@ -0,0 +1,30 @@
+﻿using ModSettings;
+
+namespace AutoSurvey
+{
+	internal class Settings : JsonModSettings
+	{
+		internal static Settings Instance { get; } = new Settings();
+
+		internal static void OnLoad()
+		{
+			Settings.Instance.AddToModSettings("Auto Map Survey");
+			Settings.Instance.RefreshGUI();
+		}
+
+		[Section("Main")]
+		[Name("Automatically survey map (without coal)")]
+		[ModSettings.Description("")]
+		public bool autodrawEnabled;
+
+		[Name("Automatically reveal map delay (s)")]
+		[ModSettings.Description("")]
+		[Slider(5f, 100f, NumberFormat = "{0:F1}")]
+		public float autodrawDelay = 10f;
+
+		[Name("Automatically reveal map range multiplier")]
+		[ModSettings.Description("")]
+		[Slider(0f, 25f, NumberFormat = "{0:F1}")]
+		public float drawingRange = 1f;
+	}
+}
